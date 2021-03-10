ccbbb6bf6fbd3847cb6ebcdba5d16bb2a72f964d
Leonardo Pilatti <leonardopilatti14@gmail.com>
2021-03-10 17:28:53 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Produto from './Produto';
 
 function App() {
+  const [produto, setProduto] = React.useState(null);
+
+  React.useEffect(() => {
+    const produtoLocal = window.localStorage.getItem('produto'); /// eu defini a chave como produto no setItem
+    if (produtoLocal !== null) setProduto(produtoLocal);
+    //console.log(produtoLocal);
+  }, []);
+
+  React.useEffect(() => {
+    if (produto !== null) window.localStorage.setItem('produto', produto); /// aqui estou dizendo que essa função sempre irá ocorrer quando o 'produto' mudar e quando mudar, ele irá salvar a preferência no localStorage do navegador; para acessar o localStorage eu vou em aplication -> localStorage no console do navegador;
+  }, [produto]);
+
+  function handleClick({ target }) {
+    // console.log(event.target.innerText);
+    setProduto(target.innerText);
+    //console.log(produto);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Preferência: {produto}</h1>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+      <Produto produto={produto} />
     </div>
   );
 }
